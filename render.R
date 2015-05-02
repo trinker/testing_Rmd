5fbbf44b477334951f6479da65424a84b503b1db
Tyler <tyler.rinker@gmail.com>
2015-05-01 20:53:41 -0400

    first commit

---
+++ render.R
@@ -0,0 +1,14 @@
+rmarkdown::render("testing_Rmd.Rmd", "all") 
+	
+
+
+md_toc <- function(path = {(x <- dir())[tools::file_ext(x) == "md"]}){
+    x <- suppressWarnings(readLines(path))
+    inds <- 1:(which(!grepl("^\\s*-", x))[1] - 1)
+    temp <- gsub("(^[ -]+)(.+)", "\\1", x[inds])
+    content <- gsub("^[ -]+", "", x[inds])
+    x[inds] <- sprintf("%s[%s](#%s)", temp, content, gsub("\\s", "-", tolower(content)))
+    cat(paste(x, collapse = "\n"), file = path)
+}
+
+md_toc()
